5e5fff7001e8924a4a5ff8eccd118e5443e3097a
Ron Kang <ronjinhong.kang@my.jcu.edu.au>
2018-12-13 13:39:06 +0800

    Begin making database on Github

--- MobileBankingSolutionDraft/runserver.py
+++ MobileBankingSolutionDraft/runserver.py
@@ -6,6 +6,7 @@ from os import environ
 from MobileBankingSolutionDraft import app
 
 if __name__ == '__main__':
+    #Test for database
     HOST = environ.get('SERVER_HOST', 'localhost')
     try:
         PORT = int(environ.get('SERVER_PORT', '5555'))
