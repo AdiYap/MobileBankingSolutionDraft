08c1b5df4464e871a5e4df12cc60b51f5193a714
Ron Kang <ronjinhong.kang@my.jcu.edu.au>
2018-12-13 14:04:08 +0800

    rename branch

--- MobileBankingSolutionDraft/runserver.py
+++ MobileBankingSolutionDraft/runserver.py
@@ -6,7 +6,7 @@ from os import environ
 from MobileBankingSolutionDraft import app
 
 if __name__ == '__main__':
-    #Test for database
+    #Test for databaseV2
     HOST = environ.get('SERVER_HOST', 'localhost')
     try:
         PORT = int(environ.get('SERVER_PORT', '5555'))
