0eee60472e8c5c57970f7e0bb666981d0841fc75
Adi Yap <adi.yap@my.jcu.edu.au>
2018-12-18 14:05:31 +0800

    Add basic firebase database functionality

--- MobileBankingSolutionDraft/MobileBankingSolutionDraft/views.py
+++ MobileBankingSolutionDraft/MobileBankingSolutionDraft/views.py
@@ -2,12 +2,16 @@
 Routes and views for the flask application.
 """
 
+
 from datetime import datetime
 from flask import render_template
 from MobileBankingSolutionDraft import app
 
 @app.route('/')
 @app.route('/home')
+
+
+
 def home():
     """Renders the home page."""
     return render_template(
